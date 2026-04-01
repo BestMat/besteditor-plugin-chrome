0a6f2b41afe6eb203483cbadcc0c2f4349297334
BestMat <tayoyuva@gmail.com>
2026-04-01 15:30:26 +0530

    Added styles to the elements in chrome.js

--- chrome.js
+++ chrome.js
@@ -5,10 +5,23 @@
 const addressBar = document.createElement("input");
 addressBar.placeholder = "Enter web URL";
 addressBar.className = "address-bar";
+addressBar.style.padding = "10px";
+addressBar.style.fontFamily = "Monaco, monospace";
+addressBar.style.borderRadius = "10px";
+addressBar.style.border = "none";
+addressBar.style.outline = "none";
 
 const addressBtn = document.createElement("button");
-addressBtn.innerText = "Go";
+addressBtn.innerHTML = `<i class="bi bi-search"></i>Browse`;
 addressBtn.className = "address-btn";
+addressBtn.style.padding = "10px";
+addressBtn.style.fontFamily = "Monaco, monospace";
+addressBtn.style.backgroundColor = "#DDD";
+addressBtn.style.color = "#181A1F";
+addressBtn.style.border = "none";
+addressBtn.style.marginLeft = "10px";
+addressBtn.style.borderRadius = "10px";
+addressBtn.style.cursor = "pointer";
 
 const webview = getWebview();
 
@@ -17,6 +30,9 @@ document.querySelector("body").appendChild(addressBtn);
 
 document.querySelector(".address-btn").addEventListener("click", async () => {
     // TODO: Validate input URL
-    const url = document.querySelector(".address-bar").value;
+    let url = document.querySelector(".address-bar").value;
+    if (!url.includes("://")) {
+        url = `https://${url}`;
+    }
     await webview.loadURL(url);
 });
